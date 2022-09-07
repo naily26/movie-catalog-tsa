232e6d4a573a131eabfa0de59bd79bc0f79f6c92
naily26 <nailymasruri@gmail.com>
2022-09-07 20:50:54 +0700

    detail movie

--- lib/pages/movie_detail.dart
+++ lib/pages/movie_detail.dart
@@ -3,21 +3,25 @@ import 'package:movie_catalog_app/models/movie.dart';
 
 class MovieDetail extends StatelessWidget {
   final Movie movie;
-  final String imgPath = 'https://image.tmbd.org/t/p/w500/';
+  final String imgPath = 'https://image.tmdb.org/t/p/w500/';
   MovieDetail(this.movie);
 
   @override
   Widget build(BuildContext context) {
     String path;
     if(movie.posterPath != null) {
-      path = imgPath + movie.posterPath;
+      var posterPath = movie.posterPath;
+      path = imgPath + posterPath!;
     } else {
-      path = 'https://images.freeimges.com/images/large-previews/5eb/movie-clapboard-1184339.jpg';
+      path = 'https://images.freeimages.com/images/large-previews/5eb/movie-clapboard-1184339.jpg';
     }
     double height = MediaQuery.of(context).size.height;
+    var title2 = movie.title;
+    var movie2 = movie;
+    var overview = movie2.overview;
     return Scaffold(
       appBar: AppBar(
-        title: Text(movie.title),
+        title: Text(title2!),
       ),
       body: SingleChildScrollView(
         child: Center(
@@ -29,8 +33,8 @@ class MovieDetail extends StatelessWidget {
                 child: Image.network(path),
               ),
               Container(
-                child: Text(movie.overview),
-                padding: EdgeInsets.only(left: 16, right:16,
+                child: Text(overview!),
+                padding: EdgeInsets.only(left: 16, right:16,)
               )
             ],
           ),
